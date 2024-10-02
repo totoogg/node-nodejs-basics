de2b6f05bd46bbf719d8b39288369f8942a7a89d
totoogg <goncharov.vova2014@gamil.com>
2024-10-02 19:59:43 +0300

    feat: add function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  try {
+    console.log(__dirname);
+    await fs.access(join(__dirname, 'files/fresh.txt'));
+    throw new Error('FS operation failed');
+  } catch (e) {
+    if (e.message !== 'FS operation failed') {
+      await fs.writeFile(
+        join(__dirname, 'files/fresh.txt'),
+        'I am fresh and young'
+      );
+    } else {
+      throw e;
+    }
+  }
 };
 
 await create();
