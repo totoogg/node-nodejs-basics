73ea1c31e371b33920a374554e7ec72dbb4a128f
totoogg <goncharov.vova2014@gamil.com>
2024-10-02 21:06:15 +0300

    feat: add function parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  console.log(
+    Object.entries(process.env)
+      .filter(([name, val]) => name.indexOf('RSS_') === 0)
+      .map((el) => `${el[0]}=${el[1]}`)
+      .join('; ')
+  );
 };
 
 parseEnv();
