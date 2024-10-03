d7b773cae8bc6d188bf255d9870e143d0d84f84f
totoogg <goncharov.vova2014@gamil.com>
2024-10-03 11:36:38 +0300

    feat: add function calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import crypto from 'crypto';
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const hash = crypto.createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(
+    join(__dirname, 'files/fileToCalculateHashFor.txt')
+  );
+  readableStream.on('readable', () => {
+    const data = readableStream.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest('hex'));
+    }
+  });
 };
 
 await calculateHash();
--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -11,42 +11,38 @@ const random = Math.random();
 
 let unknownObject;
 
-let myServer;
-
-(async function () {
-  if (random > 0.5) {
-    unknownObject = await import('./files/a.json', {
-      with: {
-        type: 'json',
-      },
-    });
-  } else {
-    unknownObject = await import('./files/b.json', {
-      with: {
-        type: 'json',
-      },
-    });
-  }
-
-  console.log(`Release ${release()}`);
-  console.log(`Version ${version()}`);
-  console.log(`Path segment separator is "${path.sep}"`);
-
-  console.log(`Path to current file is ${__filename}`);
-  console.log(`Path to current directory is ${__dirname}`);
-
-  myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+if (random > 0.5) {
+  unknownObject = await import('./files/a.json', {
+    with: {
+      type: 'json',
+    },
   });
-
-  const PORT = 3000;
-
-  console.log(unknownObject);
-
-  myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+} else {
+  unknownObject = await import('./files/b.json', {
+    with: {
+      type: 'json',
+    },
   });
-})();
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
 
 export { unknownObject, myServer };
