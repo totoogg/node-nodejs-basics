e07c4b696891f1657fcf69e9fa409e2ff22e897e
totoogg <goncharov.vova2014@gamil.com>
2024-10-03 13:02:35 +0300

    feat: add function compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'node:zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const readStream = fs.createReadStream(
+  join(__dirname, 'files/fileToCompress.txt'),
+  'utf-8'
+);
+
+const writeStream = fs.createWriteStream(join(__dirname, 'files/archive.gz'));
+
+const gzip = createGzip();
+
 const compress = async () => {
-    // Write your code here 
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
