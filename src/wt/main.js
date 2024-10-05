d4a717d2b256f7649d9f30772bb4162cc436afa1
totoogg <goncharov.vova2014@gamil.com>
2024-10-05 21:44:57 +0300

    feat: add function perfomCalculations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,54 @@
+import { Worker } from 'worker_threads';
+import os from 'node:os';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const countCore = os.availableParallelism();
+
+  const result = [];
+
+  let count = 0;
+
+  function updateResult() {
+    const showResult = result
+      .sort((a, b) => a.id - b.id)
+      .map((el) => ({ status: el.status, data: el.data }));
+
+    console.log(showResult);
+    process.exit();
+  }
+
+  Promise.all(
+    new Array(countCore).fill().map((el, i) => {
+      return new Promise((resolve, rejects) => {
+        const worker = new Worker(join(__dirname, './worker.js'), {
+          workerData: { num: i + 10, id: i },
+        });
+        worker.on('online', () => resolve(worker));
+        worker.on('message', (a) => {
+          result.push({ ...a, status: 'resolved' });
+          count += 1;
+          if (count === countCore) {
+            updateResult();
+          }
+        });
+        worker.on('error', () => {
+          result.push({
+            id: i,
+            status: 'error',
+            data: null,
+          });
+          count += 1;
+          if (count === countCore) {
+            updateResult();
+          }
+        });
+      });
+    })
+  );
 };
 
 await performCalculations();
