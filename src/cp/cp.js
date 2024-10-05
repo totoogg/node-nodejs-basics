e08e99861bf049bbab513b3e2b4326d922806f15
totoogg <goncharov.vova2014@gamil.com>
2024-10-05 22:25:50 +0300

    feat: add function spawnChildProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { spawn } from 'node:child_process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const cp = spawn(`node ${join(__dirname, 'files/script.js')}`, args, {
+    stdio: 'inherit',
+    shell: true,
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Argument1', 'Argument2']);
